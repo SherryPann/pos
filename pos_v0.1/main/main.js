b98ba10c79851f0fdb8a91a1fbdc5f0bbbdfa215
sherrypan <sherrypann@outlook.com>
2015-07-23 20:43:46 +0800

    pos_v0.1

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,36 +1,35 @@
 function printReceipt(inputs) {
-  var cartlist = '';
-  var bill = getbill(inputs);
-  bill.forEach(function(val) {
-    cartlist +=
-      '名称：' + val.name +
+  var bill = '';
+  var cartlist = [];
+
+  inputs.forEach(function(cartItem) {
+    getcartlist(cartItem, cartlist);
+  })
+  cartlist.forEach(function(val) {
+    bill += '名称：' + val.name +
       '，数量：' + val.count + val.unit +
       '，单价：' + val.price.toFixed(2) + '(元)' +
       '，小计：' + (val.price * val.count).toFixed(2) + '(元)\n';
   });
-  console.log('***<没钱赚商店>收据***\n' +
-    cartlist + '----------------------\n' +
-    '总计：' + getsumtotal(inputs) + '(元)\n' +
-    '**********************');
+  console.log('***<没钱赚商店>收据***\n' + bill + '----------------------\n' +
+    '总计：' + getsumtotal(cartlist) + '(元)\n' + '**********************');
 }
-// function getcartlist(items){
-//   return items.filter(function(val){
-//   })
-//
-// }
-function getbill(cartItems) {
 
-  var bill = [];
-  for (var i in cartItems) {
-    cartItems[i].count = 0;
-    cartItems.forEach(function(cartItem) {
-      if (cartItems[i].name === cartItem.name) {
-        cartItems[i].count++;
-        return;
-      }
-    });
-  }
-  return cartItems;
+function getcartlist(cartItem, cartlist) {
+  for (var i in cartlist) {
+    if (cartItem.barcode === cartlist[i].barcode) {
+      cartlist[i].count++;
+      return;
+    }
+  };
+  cartlist.push({
+    barcode: cartItem.barcode,
+    name: cartItem.name,
+    count: 1,
+    unit: cartItem.unit,
+    price: cartItem.price
+  })
+  return cartlist;
 }
 
 function getsumtotal(val) {
